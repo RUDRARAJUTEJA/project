36c62354e077557bfe66f8ba76a1109f3ef371b6
RUDRARAJUTEJA <116783689+RUDRARAJUTEJA@users.noreply.github.com>
2022-10-28 10:58:15 +0530

    Add files via upload

---
+++ arrays.java
@@ -0,0 +1,37 @@
+package selenium;
+
+
+
+import org.testng.annotations.Test;
+@Test
+public class arrays {
+	
+	
+	public void name()
+	{
+	char a[]= {'b','c','d','e','f'};
+	
+	for (int i = 0; i < a.length; i++) {
+	
+		
+		System.out.println(a[i]);
+	}
+	
+	}	
+	
+
+		
+	
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+}
